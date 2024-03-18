db6b0fe6cc90928a3efb2e75eb4b28d3efd79940
起个名字要半天 <1343706166@qq.com>
2024-03-18 22:12:50 +0800

    用户后台添加密码加密

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+# # Register your models here.
 from apps.users.models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['username','mobile','password']
+class UserAdmin(UserAdmin):
+    list_display = ['username','mobile','password','is_superuser']
+    search_fields = ['username']
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", 'mobile'),
+            },
+        ),
+    )
